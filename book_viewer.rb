c84840df5859a97d1c8415ce9664430d6141b12a
Jacob Resnick <jresnick3@Jacobs-MacBook-Air.local>
2019-04-16 10:20:30 -0400

    Work in book_viewer application

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,15 +2,73 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraph(string)
+    string.split("\n\n").map.with_index do |paragraph, index|
+    "<p id=\"#{index}\">#{paragraph}</p>"
+    end
+  end
+
+  def display_paragraph(string)
+    in_paragraph(string).join
+  end
+
+  def bold_match(query, paragraph)
+    paragraph.gsub(query, "<strong>#{query}</strong>")
+  end
+end
+
+
+not_found do
+  redirect "/"
+end
+
 get "/" do
-  @chapters = File.readlines("data/toc.txt")
-  @title = "Sherlock Holmes"
+  @title = "The Adventures of Sherlock Holmes"
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @chapters = File.readlines("data/toc.txt")
-  @text = File.read("data/chp1.txt")
+get "/chapters/:number" do
+  @number = params[:number].to_i
+
+  redirect "/" if @number > @contents.size
+
+  @title = "Chapter #{@number}: #{@contents[@number - 1]}"
+  @chapter = File.read("data/chp#{@number}.txt")
   erb :chapter_view
+end
+
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = Hash.new([])
+
+  return results if !query || query.empty?
+
+  each_chapter do |number, name, contents|
+    in_paragraph(contents).each_with_index do |paragraph, index|
+      if paragraph.include?(query)
+        paragraph = bold_match(query, paragraph)
+        results[name] += [{chapter: number, par_num: index, content: paragraph}]
+        puts results
+      end
+    end
+  end
+
+  results
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
 end
